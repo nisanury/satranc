95740003f2a2f74698de44062f18f336ccf07942
nisanury <nisanuryucel@gmail.com>
2021-05-11 20:57:14 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace satranc
+{
+    public class Program
+    {
+        static string[][] arr = new string[8][];
+        static Dictionary<char, float> value = new Dictionary<char, float> { { 'p', 1 }, { 'a', 3 }, { 'f', 3 }, { 'k', 5 }, { 'v', 9 }, { 's', 100 } };
+
+        static void EvaluateKnightPositions(int i, int j, char c, ref float score) //at için hesaplama
+        {
+            //2 yukarı 1sağ
+            if (i > 1 && j < 7 && arr[i - 2][j + 1] != "xx" && arr[i - 2][j + 1][1] != c)
+            {
+                score -= value[arr[i - 2][j + 1][0]] / 2;
+            }
+            //2yukarı 1 sol
+            if (i > 1 && j > 0 && arr[i - 2][j - 1] != "xx" && arr[i - 2][j - 1][1] != c)
+            {
+                score -= value[arr[i - 2][j - 1][0]] / 2;
+            }
+            //2 aşağı 1 sol
+            if (i < 6 && j > 0 && arr[i + 2][j - 1] != "xx" && arr[i + 2][j - 1][1] != c)
+            {
+                score -= value[arr[i + 2][j - 1][0]] / 2;
+            }
+            //2 aşağı 1 sağ
+            if (i < 6 && j < 7 && arr[i + 2][j + 1] != "xx" && arr[i + 2][j + 1][1] != c)
+            {
+                score -= value[arr[i + 2][j + 1][0]] / 2;
+            }
+            //1 aşağı 2 sağ
+            if (i < 7 && j < 6 && arr[i + 1][j + 2] != "xx" && arr[i + 1][j + 2][1] != c)
+            {
+                score -= value[arr[i + 1][j + 2][0]] / 2;
+            }
+            //1 yukarı 2 sağ
+            if (i > 0 && j < 6 && arr[i - 1][j + 2] != "xx" && arr[i - 1][j + 2][1] != c)
+            {
+                score -= value[arr[i - 1][j + 2][0]] / 2;
+            }
+            //1 aşağı 2 sol
+            if (i < 7 && j > 1 && arr[i + 1][j - 2] != "xx" && arr[i + 1][j - 2][1] != c)
+            {
+                score -= value[arr[i + 1][j - 2][0]] / 2;
+            }
+            //1 yukarı 2 sol
+            if (i > 0 && j > 1 && arr[i - 1][j - 2] != "xx" && arr[i - 1][j - 2][1] != c)
+            {
+                score -= value[arr[i - 1][j - 2][0]] / 2;
+            }
+
+        }
+
+        static void EvaluateBishopPositions(int i, int j, char c, ref float score) //fil için hesaplama
+        {
+            int k, l;
+
+            //sağ aşağı çapraz
+            k = i;
+            l = j;
+            while (k < 7 && l < 7 && arr[k + 1][l + 1] == "xx") // tahta içerisinde boşluk boyunca hareket et
+            {
+                k++;
+                l++;
+            }
+            if (k < 7 && l < 7 && arr[k + 1][l + 1][1] != c && arr[k + 1][l + 1] != "xx") // karşı renk taşla karşılaşıldıysa
+            {
+                score -= value[arr[k + 1][l + 1][0]] / 2;
+            }
+
+            //sol aşağı çapraz
+            k = i;
+            l = j;
+            while (k < 7 && l > 0 && arr[k + 1][l - 1] == "xx") // tahta içerisinde boşluk boyunca hareket et
+            {
+                k++;
+                l--;
+            }
+            if (k < 7 && l > 0 && arr[k + 1][l - 1][1] != c && arr[k + 1][l - 1] != "xx") // karşı renk taşla karşılaşıldıysa
+            {
+                score -= value[arr[k + 1][l - 1][0]] / 2;
+            }
+
+            //sol yukarı çapraz
+            k = i;
+            l = j;
+            while (k > 0 && l > 0 && arr[k - 1][l - 1] == "xx") // tahta içerisinde boşluk boyunca hareket et
+            {
+                k--;
+                l--;
+            }
+            if (k > 0 && l > 0 && arr[k - 1][l - 1][1] != c && arr[k - 1][l - 1] != "xx") // karşı renk taşla karşılaşıldıysa
+            {
+                score -= value[arr[k - 1][l - 1][0]] / 2;
+            }
+
+            //sağ yukarı çapraz
+            k = i;
+            l = j;
+            while (k > 0 && l < 7 && arr[k - 1][l + 1] == "xx") // tahta içerisinde boşluk boyunca hareket et
+            {
+                k--;
+                l++;
+            }
+            if (k > 0 && l < 7 && arr[k - 1][l + 1][1] != c && arr[k - 1][l + 1] != "xx") // karşı renk taşla karşılaşıldıysa
+            {
+                score -= value[arr[k - 1][l + 1][0]] / 2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int i = 0, j = 0;
+            float black = 0, white = 0;
+            char[] ch;
+
+            /*
+                Tahta Dosya Adı		Sonuçlar
+                board1.txt		    Siyah:121 (123)   , Beyaz:118 (120)
+                board2.txt		    Siyah:132.5 (133) , Beyaz:126.5 (129)
+                board3.txt		    Siyah:57.5 (108)  , Beyaz:112.5 (115)
+
+                board3.txt senaryosunda elle hesaplama yaptığımızda siyah taşların kalan puanı 55.5((100/50)+(1/2)+(1/2)+(3/2)=52,5=> 108-52,5 = 55,5)çıkıyor.
+             */
+
+            string fileName = @"board1.txt";
+            //string fileName = @"board2.txt";
+            //string fileName = @"board3.txt";
+
+            // txt üzerinden verileri oku diziye at
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    arr[i] = line.Split(" ");
+                    i++;
+                }
+            }
+
+            //tüm siyahları ve beyaz taşları topla daha sonra tehlike de olanları kontrol edip çıkarma işlermi yap
+            for (i = 0; i < 8; i++)
+            {
+                for (j = 0; j < 8; j++)
+                {
+                    ch = arr[i][j].ToCharArray(); // piyon siyah mı beyaz mı çıkar
+
+                    if (ch[1] == 's') //piyon siyahsa
+                    {
+                        if (ch[0] == 'p')
+                            black += value['p'];
+                        if (ch[0] == 'a')
+                        {
+                            black += value['a'];
+                            EvaluateKnightPositions(i, j, ch[1], ref white);
+                        }
+                        if (ch[0] == 'f')
+                        {
+                            black += value['f'];
+                            EvaluateBishopPositions(i, j, ch[1], ref white);
+                        }
+                        if (ch[0] == 'k')
+                            black += value['k'];
+                        if (ch[0] == 'v')
+                            black += value['v'];
+                        if (ch[0] == 's')
+                            black += value['s'];
+                    }
+                    else //piyon beyazsa
+                    {
+                        if (ch[0] == 'p')
+                            white += value['p'];
+                        if (ch[0] == 'a')
+                        {
+                            white += value['a'];
+                            EvaluateKnightPositions(i, j, ch[1], ref black);
+                        }
+                        if (ch[0] == 'f')
+                        {
+                            white += value['f'];
+                            EvaluateBishopPositions(i, j, ch[1], ref black);
+                        }
+                        if (ch[0] == 'k')
+                            white += value['k'];
+                        if (ch[0] == 'v')
+                            white += value['v'];
+                        if (ch[0] == 's')
+                            white += value['s'];
+                    }
+                }
+            }
+            Console.WriteLine($"Siyah: {black} | Beyaz: {white}");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,40 @@
+#Problem Tanımı
+##Satranç Problemi
+~~Satranç tahtası üzerinde bulunan taşlara göre iki tarafın (siyah – beyaz) mevcut durumlarının puan hesaplaması.~~
+
+#####Açıklama
+Mevcut puanı hesaplama algoritması şöyledir:
+1. Bir taşın tehdit altında olup olmadığının kontrolü, o taşı tehdit eden karşı renkte bir veya birden fazla taş olması durumunda oluşur.
+2. Table 1 Satranç TaşlarıTable 1’de taşların puanları verilmiştir. Eğer bir taş tehdit edilmiyorsa tablodaki puanı alır.
+3. Eğer bir taş karşı tarafın taşlarından herhangi biri tarafından tehdit ediliyorsa, tehdit edilen taşın puanı Table 1’deki puanının yarısı alınır.
+4. At ve Fil taşlarının tehdit durumları kontrol edilir. Siyah ve beyaz taşlar için iki ayrı puan hesaplanır.
+
+~~Satranç Taşları ve Puanları~~
+| Taş İsmi | Kısaltma | Puan |
+| --------- | -------- | --- |
+| Piyon | p | 1 |
+| At | a | 3 |
+| Fil | f | 3 |
+| Kale | k | 5 |
+| Vezir | v | 9 |
+| Şah | s | 100 |
+
+##Örnek
+![Tahta_1](.\board1.png)
+~~Figür 1 Tehdit Durumu~~
+Siyah:(1*0,5)+(7*1)+(1*3)+(1*3)+(2*5)+9+100 = 132,5
+Beyaz:(2*0,5)+(5*1)+(1*1,5)+(2*5)+9+100 = 126,5
+
+Program girdileri
+Figür 1 tahtasının formatı:
+
+ks xx xx vs xx xx xx ks
+xx xx ps fs xx xx xx xx
+ps xx ss xx ps xx xx ps
+xx ps pb ps xx xx ps xx
+pb xx xx pb ps xx xx xx
+xx xx xx xx as xx pb xx
+xx xx pb xx xx xx pb pb
+kb vb xx xx sb fb xx kb
+
+Burada ilk harf taş bilgisini (Tabloda verilmiştir.) ikinci harf ise renk bilgisini, xx ile ifade edilen yerler ise tahtadaki boş alanları göstermektedir.
